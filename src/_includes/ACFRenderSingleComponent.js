bc949809ffa527cfd6826fc4e052d9d98e8bb2de
sara <sara@dpi.nl>
2023-09-07 12:47:12 +0200

    ACFRenderSingleComponent.js

--- src/_includes/ACFRenderSingleComponent.js
+++ src/_includes/ACFRenderSingleComponent.js
@@ -5,39 +5,27 @@ import ComponentCTA from "../components/ComponentCTA/ComponentCTA";
 
 const componentMap = {
     component_hero: ComponentHero,
-    component_cta : ComponentCTA
+    component_cta: ComponentCTA
     // Add more entries for other component types...
 };
 
 const ACFRenderSingleComponent = ({ pageId, componentName }) => {
-    console.log(pageId)
     const [acfData, setAcfData] = useState(null);
-    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
     useEffect(() => {
-        // Fetch ACF data using the pageId
-        fetchData(pageId);
+        axios.get(`http://headless-wp.test/wp-json/wp/v2/pages/${pageId}`)
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error(`Request failed with status ${response.status}`);
+                }
+                setAcfData(response.data.acf);
+            })
+            .catch(err => {
+                console.error('Error fetching ACF data:', err);
+                setError(err); // Set error state with more details
+            })
+
     }, [pageId]);
-
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`http://headless-wp.test/wp-json/wp/v2/pages/${pageId}`);
-
-            setAcfData(response.data.acf);
-
-            setLoading(false);
-
-
-        } catch (error) {
-            setError(error);
-            setLoading(false);
-        }
-    };
-
-    if (loading) {
-        return <p>Loading...</p>;
-    }
 
     if (error) {
         return <p>Error: {error.message}</p>;
@@ -45,9 +33,12 @@ const ACFRenderSingleComponent = ({ pageId, componentName }) => {
 
     const SingleComponent = componentMap[componentName];
 
-    return SingleComponent && acfData && acfData[componentName] ? (
-        <SingleComponent {...acfData[componentName][0]} />
-    ) : null;
+    if (SingleComponent && acfData && acfData[componentName]) {
+        const componentData = acfData[componentName][0];
+        return <SingleComponent {...componentData} />;
+    }
+
+    return null;
 };
 
 export default ACFRenderSingleComponent;
