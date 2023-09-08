fa26f28513bce0dfa901c4ec4b4576b3c6551d99
sara <sara@dpi.nl>
2023-09-08 21:34:27 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Route as RoutedRoute } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route as RoutedRoute} from 'react-router-dom';
 
-import GetCurrentPageId from "./_includes/GetCurrentPageId";
-import ACFRenderSingleComponent from "./_includes/ACFRenderSingleComponent";
-import ACFRenderFlexibleContent from './_includes/ACFRenderFlexibleContent';
 import SinglePage from './templates/SinglePage';
 import ComponentHeader from "./components/ComponentHeader/ComponentHeader";
-import PageArchive from "./templates/PageArchive";
+import ArchivePage from "./templates/ArchivePage";
+import HomePage from "./templates/HomePage"
 
 function App() {
-
-  const [homepageId, setHomepageId] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetchHomepageData();
-  }, []);
-
-  const fetchHomepageData = async () => {
-    try {
-      const response = await axios.get('http://headless-wp.test/wp-json/wp/v2/pages');
-      const homepage = response.data.find(page => page.slug === 'home');
-
-      if (homepage) {
-        setHomepageId(homepage.id);
-      }
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      setLoading(false);
-    }
-  };
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-  return (
-      <Router>
-        <ComponentHeader />
-        <Routes>
-          // Show single page (detail page)
-          <RoutedRoute path="/page/:slug" element={<SinglePage />} />
-          // Show overview pages
-          <RoutedRoute path="/pages" element={<PageArchive />} />
-          // Homepage
-          <RoutedRoute path="/" element={
-            <>
-              <ACFRenderSingleComponent pageId={homepageId} componentName={'component_hero'} />
-              <ACFRenderFlexibleContent pageId={homepageId} />
-            </>
-          } />
-
-          {/*<RoutedRoute path="acf/:id"*/}
-          {/*             element={<GetCurrentPageId>*/}
-          {/*               {(currentPageId) => <ACFRenderFlexibleContent pageId={currentPageId} />}*/}
-          {/*             </GetCurrentPageId>}/>*/}
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <ComponentHeader/>
+            <Routes>
+                {/* Homepage */}
+                <RoutedRoute path="/" element={<HomePage/>}/>
+                {/* Single page (detail page) */}
+                <RoutedRoute path="/:slug" element={<SinglePage/>}/>
+                {/* Archive page */}
+                <RoutedRoute path="/pages" element={<ArchivePage/>}/>
+            </Routes>
+        </Router>
+    );
 }
+
 export default App;
