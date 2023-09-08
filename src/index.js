c354331763c544153efee1dce6eb2aa2e0a63d92
sara <sara@dpi.nl>
2023-09-08 21:35:39 +0200

    add global variables to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/main.scss'; // Import your stylesheet
 
+// Global variables
+window.baseUrl = 'http://headless-wp.test/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
