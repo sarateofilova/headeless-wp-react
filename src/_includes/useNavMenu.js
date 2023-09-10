8513dcd975723921e77e8c020cae7091662cbbb0
sara <sara@dpi.nl>
2023-09-10 23:17:04 +0200

    useNavMenu.js

---
+++ src/_includes/useNavMenu.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useLocation } from 'react-router-dom';
+import useHomepageId from "./useHomepageId";
+
+function useNavMenu({apiUrl}) {
+
+    const [menuData, setMenuData] = useState([]);
+    const { homepageId } = useHomepageId();
+
+    const fetchMenuData = async () => {
+        try {
+            const response = await axios.get(apiUrl);
+            const menuData = response.data;
+
+            // Set the menu data to the state
+            setMenuData(menuData);
+        } catch (error) {
+            console.error('Error fetching menu data:', error);
+        }
+    };
+
+    useEffect(() => {
+        // Fetch menu data when the component mounts
+        fetchMenuData();
+    });
+
+    return (
+        <MenuComponent menuData={menuData} homepageId={homepageId} />
+    );
+}
+
+function MenuComponent({ menuData, homepageId }) {
+    // Create a mapping of parent items by their ID
+    const parentItemMap = {};
+    menuData.forEach((menuItem) => {
+        if (menuItem.menu_item_parent !== '0') {
+            const parentId = menuItem.menu_item_parent;
+            if (!parentItemMap[parentId]) {
+                parentItemMap[parentId] = [];
+            }
+            parentItemMap[parentId].push(menuItem);
+        }
+    });
+
+    // Function to recursively render menu items with their children
+    function RenderMenuItem(menuItem, customItemClass, customLinkClass) {
+        const location = useLocation();
+
+        const hasChildren = (parentItemMap[menuItem.ID] && parentItemMap[menuItem.ID].length > 0) ?? false;
+        const isSubMenu = (menuItem.menu_item_parent !== '0');
+        const itemClassName = hasChildren ? `menu-item has-children` : `${customItemClass ?? 'menu-item'}`;
+        const linkClassName = isSubMenu ? 'sub-menu-item-link' : `${customLinkClass ?? 'menu-item-link'}`;
+        const activeClass = (location.pathname === `/${menuItem.slug}` ? 'active' : '');
+
+        return (
+            <li className={`${itemClassName} ${activeClass}`} key={menuItem.ID}>
+                <Link
+                    key={menuItem.ID + 1} to={menuItem.object_id === `${homepageId}` ? '/' : `/${menuItem.slug}`}
+                    className={linkClassName}
+                >
+                    {menuItem.title}
+                </Link>
+                {hasChildren && (
+                    <ul className="sub-menu">
+                        {parentItemMap[menuItem.ID].map((childItem) => RenderMenuItem(childItem, 'sub-menu-item', 'sub-menu-link'))}
+                    </ul>
+                )}
+            </li>
+        );
+    }
+
+
+    // Render top-level menu items (items without a parent)
+    const topLevelMenuItems = menuData.filter((menuItem) => menuItem.menu_item_parent === '0');
+
+    return (
+        <div>
+            <ul className="nav-menu">
+                {topLevelMenuItems.map((menuItem) => RenderMenuItem(menuItem))}
+            </ul>
+        </div>
+    );
+}
+
+export default useNavMenu;
